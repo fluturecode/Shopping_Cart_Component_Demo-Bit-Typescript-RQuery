87f8606c37fe3af447abc429f1e00f834de8e61e
Elizabeth Eidelson <fluturecode@gmail.com>
2021-02-27 00:21:23 -0500

    updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import LinearProgress from "@material-ui/core/LinearProgress"
+import { useQuery } from "react-query"
+// Types
+export type CartItemType = {
+	id: number
+	category: string
+	description: string
+	image: string
+	price: number
+	title: string
+	amount: number
+}
+
+const getProducts = async (): Promise<CartItemType[]> =>
+	await (await fetch("https://fakestoreapi.com/products")).json()
 const App = () => {
-	return (
-		<>
-			<h1>Shopping Cart</h1>
-		</>
+	const { data, isLoading, error } = useQuery<CartItemType[]>(
+		"products",
+		getProducts
 	)
+	const getTotalItems = () => null
+
+	const handleAddToCart = () => null
+
+	const hanleRemoveCart = () => null
+
+	if (isLoading) return <LinearProgress />
+	if (error) return <div>You're app is crashing</div>
+
+	return <div>Shopping Cart</div>
 }
 
 export default App
