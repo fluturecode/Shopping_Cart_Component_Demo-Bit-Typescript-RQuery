89104dc7c24b0a4d732f982f7e08aa91de87c3a8
Elizabeth Eidelson <fluturecode@gmail.com>
2021-02-27 23:22:02 -0500

    updated App.tsx;

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import Grid from "@material-ui/core/Grid"
 import LinearProgress from "@material-ui/core/LinearProgress"
 import { useQuery } from "react-query"
+import { Wrapper } from "./App.styles"
+import Item from "./Item/Item"
 // Types
 export type CartItemType = {
 	id: number
@@ -13,21 +16,34 @@ export type CartItemType = {
 
 const getProducts = async (): Promise<CartItemType[]> =>
 	await (await fetch("https://fakestoreapi.com/products")).json()
+
 const App = () => {
 	const { data, isLoading, error } = useQuery<CartItemType[]>(
 		"products",
 		getProducts
 	)
+	console.log(data)
+
 	const getTotalItems = () => null
 
-	const handleAddToCart = () => null
+	const handleAddToCart = (clickedItem: CartItemType) => null
 
 	const hanleRemoveCart = () => null
 
 	if (isLoading) return <LinearProgress />
 	if (error) return <div>You're app is crashing</div>
 
-	return <div>Shopping Cart</div>
+	return (
+		<Wrapper>
+			<Grid container spacing={3}>
+				{data?.map((item) => (
+					<Grid item key={item.id} xs={12} sm={4}>
+						<Item item={item} handleAddToCart={handleAddToCart} />
+					</Grid>
+				))}
+			</Grid>
+		</Wrapper>
+	)
 }
 
 export default App
